9c3033c7cc535feff86654dfaaf32dc2fe1839a7
Keegan Holley <keegan2149@gmail.com>
2014-11-06 00:10:52 -0500

    snmp query and processing working. Starting on report

--- icstatus.py
+++ icstatus.py
@@ -10,14 +10,15 @@ class snmp_get_response(object):
 		self.base_oid = ''
 		self.base_oid_name = ''
 		self.table_oid = ''
+		self.table_oid_name = ''
 		self.type = ''
 		self.values = {}
 		self.size = 0
 
 	def append_data(self,input_list=[]):
 		index = int(input_list[0].lstrip('.')) 
-		if not self.values.has_key(ident):
-			self.values[ident] = ' '.join(input_list[3::]).rstrip()
+		if not self.values.has_key(index):
+			self.values[index] = ' '.join(input_list[3::]).rstrip()
 			self.type = input_list[2].rstrip(':')
 			self.size = len(self.values.keys())
 		else:
@@ -45,27 +46,32 @@ def do_get_from_shell(device,snmp_info = {},table='no'):
 		names = None
 		result_dict = {}
 		for line in result:
-			if names = None:
-				if re.search(snmp_info[base_oid],line,re.I):
+			line = line.rstrip()
+			if names == None:
+				if re.search(snmp_info['base_oid'],line,re.I):
 					names = False
-				elif re.search(snmp_info[base_oid_name],line,re.I):
+				elif re.search(snmp_info['base_oid_name'],line,re.I):
 					names = True
-			if names = True:
-				base_oid = snmp_info[base_oid_name]
-			elif names = False:
-				base_oid = snmp_info[base_oid]
-			for ident,value_name in snmp_info[colunms].items():
-				pattern = base_oid + table_oid + '.' + str(ident)
+			if names == True:
+				base_oid = snmp_info['base_oid_name']
+			elif names == False:
+				base_oid = snmp_info['base_oid']
+			table_oid = snmp_info['table_oid']
+			for ident,value_name in snmp_info['colunms'].items():
+				pattern = '^' + base_oid + table_oid + '\.' + str(ident) + '\.'
+				substitution = base_oid + table_oid + '.' + str(ident)
 				reg_search = re.compile(pattern)
 				if reg_search.search(line):
-					temp = re.sub(pattern,'',line).split(' ')
+					temp = re.sub(substitution,'',line).split(' ')
 					if not result_dict.has_key(value_name):
 						result_dict[value_name] = snmp_get_response()
-						result_dict[value_name].oid = snmp_info[oid]
-						result_dict[value_name].base_oid = snmp_info[base_oid]
-						result_dict[value_name].base_oid_name = snmp_info[base_oid_name]
-						result_dict[value_name.].table_oid = snmp_info[table_oid]
+						result_dict[value_name].oid = snmp_info['oid']
+						result_dict[value_name].base_oid = snmp_info['base_oid']
+						result_dict[value_name].base_oid_name = snmp_info['base_oid_name']
+						result_dict[value_name].table_oid = snmp_info['table_oid']
+						result_dict[value_name].table_oid_name = value_name
 					result_dict[value_name].append_data(input_list=temp)
+	return result_dict
 
 
 
@@ -96,15 +102,34 @@ snmp_info = {
 		15: "device raid",
 		16: "device firmware",
 		17: "device smarthealth",
-		18: "device smarthealth",
-		19: "storage device",
-		20: "storage device",
-		90: "storagedevice state",
-		91: "storage device",
+		18: "device smarthealth status",
+		19: "storage device capacity",
+		20: "hot removable",
+		90: "storage device state",
+		91: "storage device status",
 	}
 }
 
-import pdb ; pdb.set_trace()
-result = do_get_from_shell("san01",oid,table='yes',columns=colunms_dict)
+
+
+heading = '|%(heading1)s|%(heading2)s|%(heading3)s|%(heading4)s|%(heading5)s|%(heading6)s|%(heading7)s|%(heading8)s|%(heading9)s|%(heading10)s|' % { 'heading1':'  drive name  ', 'heading2':'  drive class  ', 'heading3':'  drive mode  ', 'heading4':'  drive serial  ' , 'heading5':'  temp  ' , 'heading6':'  array  ' , 'heading7':'  smart health  ' , 'heading8':'  capacity  ' , 'heading9':'  device state  ' , 'heading10':'  device status  ' }
+horizontal_border = '=' * len(heading)
+title = '=' * len(heading)/2 + '  ' + device + '  ' + '=' * len(heading)/2 
+cell_size = 10
+print horizontal_border
+print title
+for 
+
+
+
+results_dict = do_get_from_shell("san01",snmp_info=snmp_info,table='yes')
+
+drive_dict = {}
+drive_names = results_dict['device name'].values
+number_of_drives = len(drive_names.keys())
+for index,current_drive in sorted(drive_names):
+	print
+
+pass
 
 import pdb ; pdb.set_trace()
