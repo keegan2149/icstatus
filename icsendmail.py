1f8efd09c32eff478a0c3132e1dc18a9a739b5b1
Keegan Holley <keegan2149@gmail.com>
2014-11-10 05:26:56 -0500

    added inline css to email and updated cell generator to handle prtg data

--- icsendmail.py
+++ icsendmail.py
@@ -15,17 +15,18 @@ def ic_sendmail(sender_address = 'status-automation@ixlink.com',sender_name = 'r
 	receiver_addresses = ', '.join(receiver_info.values())
 	receiver_addresses.rstrip(', ')
 
-	if isinstance(payload,list):
-		body = MIMEText(''.join(payload), 'plain')
-	elif isinstance(payload,str):
-		body = MIMEText(payload, 'plain')
+	txt_body = MIMEText(''.join(str_message), 'plain')
+	html_body = MIMEText(generate_html_message(message_lines=list_message), 'html')
+
+	print html_body
 
 	msg = MIMEMultipart('alternative')
 	msg['Subject'] = subject
 	msg['From'] = sender_address
 	msg['To'] = receiver_addresses
 	msg.preamble = subject
-	msg.attach(body)
+	msg.attach(txt_body)
+	msg.attach(html_body)
 
 	try:
 		smtpObj = smtplib.SMTP('localhost')
@@ -36,7 +37,7 @@ def ic_sendmail(sender_address = 'status-automation@ixlink.com',sender_name = 'r
 		print e
 		print "Error: unable to send email"
 		return 1
-		
+
 def ic_mime_mail(sender_address = 'status-automation@ixlink.com',sender_name = 'rancid-centos',receiver_info= [], subject='',body='',pics=[]):
 
 	receiver_list = ''
@@ -66,42 +67,42 @@ def ic_mime_mail(sender_address = 'status-automation@ixlink.com',sender_name = '
 
 
 def generate_html_message(message_lines=[],layout='html_table'):
-	css = """
-table, th, td {
-	border: 1px solid black;
-}
-
-	"""
-	def generate_cells(line = [],indent=0,header=False):
+	css = """\t\t<style type="text/css">
+\t\t\ttable, th { border: 1px solid black; background-color: #DCDCDC;}
+\t\t\ttd { font-family: Verdana, Georgia, Arial, sans-serif;font-size: 9px;color:#000; border: 1px solid black;background-color: #DCDCDC;}
+\t\t\ttd.prtg { font-family: Verdana, Georgia, Arial, sans-serif;font-size: 8px;color:#000;background-color: #DCDCDC;}
+\t\t\ttd.device { "font-family: Verdana, Georgia, Arial, sans-serif;font-size: 12px;color:#000; font-weight: 9000;text-align: center;background-color: #DCDCDC;"}
+\t\t</style>
+"""
+	def generate_cells(line = [],indent=0,line_type=''):
 		cells = ''
-		if header:
+		if line_type == 'header':
 			for item in line:
-				cells += '\t' * indent + '<th> ' + item + r'<\th>\n'
+				cells += '\t' * indent + '<th style="border: 1px solid black;background-color: #DCDCDC;text-align: center;"> ' + item + '</th>' + '\n'
+		elif line_type == 'device': 
+			for item in line:
+				cells += '\t' * indent + '<td colspan="100"  style="font-family: Verdana, Georgia, Arial, sans-serif;font-size: 12px;color: #000;border: 1px solid black;background-color: #DCDCDC;font-weight: 900;text-align: center;" >' + item + '</td>' + '\n'
 		else:
 			for item in line:
-				cells += '\t' * indent + '<td>' + item + r'<\td>\n'
+				cells += '\t' * indent + '<td style="font-family: Verdana, Georgia, Arial, sans-serif;font-size: 9px;color: #000;border: 1px solid black;background-color: #DCDCDC;text-align: center;">' + item + '</td>' + '\n'
 		return cells
 
 	if layout == 'html_table':
-		message = '<html>\n\t<head><\head>\n\t\t<body>\n\t\t\t<table>\n'
+		message = '<html>\n\t<head>' + css + '\n\t\t<meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\'/>\n\t\t<meta http-equiv=\'X-UA-Compatible\' content=\'IE=edge,chrome=1\'/>\n\t</head>\n\t\t<body>\n\t\t\t<table style="border: 1px solid black;background-color: #DCDCDC">\n'
 		indent = 4
 		tab = '\t'
 		for line in message_lines:
 			for line_type,list_line in line.items():
 				message += '\t' * indent + '<tr>\n'
-				if line_type == 'device' or line_type == 'header':
-					header=True
-				else:
-					header=False
 				indent += 1
-				message += generate_cells(list_line,indent=indent,header=header)
+				message += generate_cells(list_line,indent=indent,line_type=line_type)
 				indent -= 1
-				message += '\t' * indent + r'<\tr>\n'
+				message += '\t' * indent + '</tr>'+ '\n'
 		indent -= 1
-		message += '\t' * indent + r'<\table>'
+		message += '\t' * indent + '</table>' + '\n'
 		indent -= 1
-		message += '\t' * indent + r'<\body>'
+		message += '\t' * indent + '</body>' + '\n'
 		indent -= 1
-		message += '\t' * indent + r'<\html>'
+		message += '\t' * indent + '</html>' + '\n'
 	return message
 
